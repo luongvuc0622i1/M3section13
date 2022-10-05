f50883d31dcee56b93850b4a552ed862a779b201
Gia Lương <gialuong@MacBook-Pro-cua-Gia.local>
2022-10-05 11:41:18 +0700

    bai tap 2

---
+++ bt2_user_manager/src/main/java/com/codegym/dao/UserDAO.java
@@ -0,0 +1,129 @@
+package com.codegym.dao;
+
+import com.codegym.model.User;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO implements IUserDAO {
+    private static final String jdbcURL = "jdbc:mysql://localhost:3306/demo?useSSL=false";
+    private static final String jdbcUsername = "root";
+    private static final String jdbcPassword = "22072022";
+
+    private static final String INSERT_USERS_SQL = "INSERT INTO users (name, email, country) VALUES (?, ?, ?);";
+    private static final String SELECT_USER_BY_ID = "select id,name,email,country from users where id =?";
+    private static final String SELECT_ALL_USERS = "select * from users";
+    private static final String DELETE_USERS_SQL = "delete from users where id = ?;";
+    private static final String UPDATE_USERS_SQL = "update users set name = ?,email= ?, country =? where id = ?;";
+
+    public UserDAO() {
+    }
+
+    private Connection getConnection() {
+        Connection c = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    @Override
+    public void insertUser(User user) throws SQLException {
+        Connection c = getConnection();
+        try {
+            PreparedStatement ps = c.prepareStatement(INSERT_USERS_SQL);
+            ps.setString(1, user.getName());
+            ps.setString(2, user.getEmail());
+            ps.setString(3, user.getCountry());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+    }
+
+    @Override
+    public User selectUser(int id) {
+        User user = null;
+        Connection c = getConnection();
+        try {
+            PreparedStatement ps = c.prepareStatement(SELECT_USER_BY_ID);
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                user = new User(name, email, country);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return user;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        List<User> users = new ArrayList<>();
+        Connection c = getConnection();
+        try {
+            PreparedStatement ps = c.prepareStatement(SELECT_ALL_USERS);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                User user = new User(id, name, email, country);
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return users;
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        boolean result;
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement(DELETE_USERS_SQL);
+        ps.setInt(1, id);
+        result = ps.executeUpdate() > 0;
+        return result;
+    }
+
+    @Override
+    public boolean updateUser(User user) throws SQLException {
+        boolean result;
+        Connection c = getConnection();
+        PreparedStatement ps = c.prepareStatement(UPDATE_USERS_SQL);
+        ps.setString(1, user.getName());
+        ps.setString(2, user.getEmail());
+        ps.setString(3, user.getCountry());
+        ps.setInt(4, user.getId());
+        result = ps.executeUpdate() > 0;
+        return result;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
+    }
+}
